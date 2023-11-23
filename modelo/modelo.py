ee15e57d626a6f2076e878fba499386fc674988a
marcsilva <marciliodevjava@gmail.com>
2023-11-23 16:51:18 -0300

    Update modelo

--- modelo/modelo.py
+++ modelo/modelo.py
@@ -4,6 +4,7 @@ class Filme:
         self.__nome = str(nome.title())
         self.__ano = int(ano)
         self.__duracao = float(duracao)
+        self.__likes = int(0)
 
     def get_filme(self):
         nome = " Nome: {}".format(self.__nome)
@@ -19,27 +20,49 @@ class Filme:
     def nome(self, nome):
         self.__nome = nome
 
+    def contador_likes(self):
+        self.__likes += 1
+
+    def informacao(self):
+        print("")
+        print("Nome: {}".format(self.__nome))
+        print("Ano: {}".format(self.__ano))
+        print("Duração? {}".format(self.__duracao))
+        print("Likes: {}".format(self.__likes))
+        return ""
+
 class Serie:
 
     def __init__(self, nome, ano, temporadas):
         self.__nome = str(nome.title())
         self.__ano = ano
         self.__temporadas = temporadas
+        self.__likes = int(0)
 
+    def contador_likes(self):
+        self.__likes += 1
 
     def informacao(self):
         print("")
         print("Nome: {}".format(self.__nome))
         print("Ano: {}".format(self.__ano))
         print(f"Temporadas: {self.__temporadas}")
+        print("Likes: {}".format(self.__likes))
         return ""
 
 
 vingadores = Filme("Vingadores - Guerra Infinita", 2021, 2.35)
 print(vingadores.nome)
+print(vingadores.informacao())
+vingadores.contador_likes()
 vingadores.nome = "Vingadores - Guerra Infinita II"
 print(vingadores.nome)
+print(vingadores.informacao())
 
 
 atlanta = Serie('Atlanta', 2018, 2)
+print(atlanta.informacao())
+atlanta.contador_likes()
+print(atlanta.informacao())
+atlanta.contador_likes()
 print(atlanta.informacao())
